500a40890af533c574942ccab5c07383319bf9e8
Juozas <juozas@ivejas.lt>
2020-03-12 20:59:33 +0200

    state hooks class

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import logo from "./logo.svg"
+import styles from "./App.module.scss"
+import cx from "classnames"
+// import "./App.scss"
+
+// class App extends React.Component {
+//     state = {
+//         logoIsShown: true
+//     }
+//     toggleImage = () => {
+//         this.setState(state => ({
+//             logoIsShown: !state.logoIsShown
+//         }))
+//     }
+//     render() {
+//         return (
+//             // <div className={`${styles.App} ${styles.appLogo}`}>
+//             <div className={cx(styles.App)}>
+//                 <header>
+//                     <img
+//                         src={logo}
+//                         className={cx(
+//                             styles.appLogo,
+//                             !this.state.logoIsShown && styles.dNone
+//                         )}
+//                         alt="logo"
+//                     />
+//                     <h1>TITLE</h1>
+//                     <p onClick={this.toggleImage}>TOGGLE IMAGE</p>
+//                     <a
+//                         className="App-link"
+//                         href="https://reactjs.org"
+//                         target="_blank"
+//                         rel="noopener noreferrer"
+//                     >
+//                         Learn React
+//                     </a>
+//                 </header>
+//                 <div>sdkjhsdhjsdhjsdsdjh</div>
+//             </div>
+//         )
+//     }
+// }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isShown, setIsShown] = useState(true)
+    const [isX, setIsX] = useState(true)
+
+    console.log("render")
+
+    const func = () => {
+        setIsShown(!isShown)
+        setIsX(!isX)
+    }
+
+    const obj = {
+        name: "Jonas",
+        surname: "Jonaitis",
+        obj2: {
+            age: 18
+        }
+    }
+
+    const { name: manoVardas, surname, obj2: { age } = {}, obj2 } = obj
+    // const state = arr[0]
+    // const setState = arr[1]
+    return (
+        // <div className={`${styles.App} ${styles.appLogo}`}>
+        <div className={cx(styles.App)}>
+            <header>
+                <img
+                    src={logo}
+                    className={cx(styles.appLogo, !isShown && styles.dNone)}
+                    alt="logo"
+                />
+                <h1>TITLE {isX ? "test" : "not"}</h1>
+                <p onClick={func}>toggle image</p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <div>sdkjhsdhjsdhjsdsdjh</div>
+        </div>
+    )
 }
 
-export default App;
+export default App
