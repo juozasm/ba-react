dd62e7bf1d2ebbd8deff9748942a4d96ca43fd69
juozasm <juozas@ivejas.lt>
2020-07-09 19:39:16 +0300

    working example

--- app/src/views/App.js
+++ app/src/views/App.js
@@ -1,25 +1,18 @@
 import React from "react"
-import { connect, useSelector, useDispatch } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import { testAction as testActionCreator } from "../store/testFeature/testActions"
 
 function App(props) {
     const testFeature = useSelector((state) => state.testFeature)
     const dispatch = useDispatch()
-    const testAction = () => dispatch(testActionCreator)
+    const testAction = () => dispatch(testActionCreator())
     console.log(testFeature, testAction, props)
-    return <div>HELLO WORLD!</div>
+    return (
+        <div>
+            HELLO WORLD! {testFeature.test}{" "}
+            <button onClick={testAction}>BOOM!</button>
+        </div>
+    )
 }
 
 export default App
-
-// const mapStateToProps = (state /*, ownProps*/) => {
-//     return {
-//         testFeature: state.testFeature,
-//     }
-// }
-
-// const mapDispatchToProps = {
-//     testAction,
-// }
-
-//export default connect(mapStateToProps, mapDispatchToProps)(App)
