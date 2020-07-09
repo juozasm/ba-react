93d4ced70120e97342f9e20be0f9a34ec618e5f1
juozasm <juozas@ivejas.lt>
2020-07-09 19:35:14 +0300

    redux hooks ver

--- app/src/views/App.js
+++ app/src/views/App.js
@@ -1,5 +1,25 @@
 import React from "react"
+import { connect, useSelector, useDispatch } from "react-redux"
+import { testAction as testActionCreator } from "../store/testFeature/testActions"
 
-export default function App() {
+function App(props) {
+    const testFeature = useSelector((state) => state.testFeature)
+    const dispatch = useDispatch()
+    const testAction = () => dispatch(testActionCreator)
+    console.log(testFeature, testAction, props)
     return <div>HELLO WORLD!</div>
 }
+
+export default App
+
+// const mapStateToProps = (state /*, ownProps*/) => {
+//     return {
+//         testFeature: state.testFeature,
+//     }
+// }
+
+// const mapDispatchToProps = {
+//     testAction,
+// }
+
+//export default connect(mapStateToProps, mapDispatchToProps)(App)
